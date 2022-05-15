551dde3750b3e0496c983c9ff2585f259390b7d3
David Callizaya <davidcallizaya@gmail.com>
2022-05-15 09:49:01 -0400

    Deploy script

--- bin/deploy.php
+++ bin/deploy.php
@@ -35,4 +35,23 @@ foreach ($files as $name => $file) {
 // Zip archive will be created only after closing object
 $zip->close();
 
-echo 'zipped folder: ' . $zip_file;
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://callizaya.com/api.php/deploy/package',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('file'=> new CURLFILE($zip_file)),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+unlink($zip_file);
