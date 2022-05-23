32ba6295534d829ea78043816c03b5898c4510ce
David Callizaya <davidcallizaya@gmail.com>
2022-05-23 15:11:09 -0400

    Remove unused code

--- src/Resources/ResourceBase.php
+++ src/Resources/ResourceBase.php
@@ -32,43 +32,6 @@ abstract class ResourceBase
         $this->definition = json_decode(file_get_contents($params['definition']), true);
     }
 
-    /**
-     * Execute a query, and return the statement
-     *
-     * Also removes the unused parameters from the $params array
-     *
-     * @param string $query
-     * @param array $options
-     *
-     * @return PDOStatement
-     */
-    protected function query($query, array $params = [])
-    {
-        try {
-            // Find :variable in $query
-            $matches = [];
-            preg_match_all('/:([a-zA-Z0-9_]+)/', $query, $matches);
-            $variables = $matches[1];
-            // Filter params by $variables
-            $params = array_filter($params, function ($key) use ($variables) {
-                return in_array($key, $variables);
-            }, ARRAY_FILTER_USE_KEY);
-
-            // $this->log($query . ': ' . json_encode($params));
-            $statement = $this->connection->prepare($query);
-            $success = $statement->execute($params);
-            if (!$success) {
-                $errorInfo = $statement->errorInfo();
-                error_log(json_encode($errorInfo));
-                throw new Exception($errorInfo[2], $errorInfo[0]);
-            }
-            return $statement;
-        } catch (Exception $e) {
-            $code = intval($e->getCode()) ?: 500;
-            throw new Exception($e->getMessage() . ' in ' . $query, $code, $e);
-        }
-    }
-
     protected function evaluate($expression, array $variables = [])
     {
         $expression = new Expression($expression, $this->request);
