9819bec9d2de78aea6655010752ceb8624179258
David Callizaya <davidcallizaya@gmail.com>
2025-10-03 23:44:09 -0400

    unmute

--- public/it-governance/en.php
+++ public/it-governance/en.php
@@ -1082,9 +1082,9 @@
         <button popovertarget="videoPop" popovertargetaction="show" class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 gap-2">Show Hint</button>
         <div id="videoPop" popover>
           <iframe
-            src="https://www.youtube.com/embed/zH85-m2rsWY?autoplay=1&mute=1&rel=0"
+            src="https://www.youtube.com/embed/zH85-m2rsWY?autoplay=1&mute=0&rel=0"
             title="YouTube video player"
-            allow="encrypted-media; picture-in-picture; fullscreen"
+            allow="autoplay; encrypted-media; picture-in-picture; fullscreen"
             allowfullscreen
           ></iframe>
         </div>
