caeffef2a8d945a38ff6201bc4c4e34c13801253
David Callizaya <davidcallizaya@gmail.com>
2025-10-11 08:11:29 -0400

    Add Google OAuth 2.0 authentication and callback handling

---
+++ microservices/auth/google.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Google OAuth 2.0 auth url 
+ */
+
+use Google\Client;
+
+$base = dirname($_SERVER['SCRIPT_URI']);
+
+$configJsonPath = realpath(getenv('GOOGLE_APPLICATION_CREDENTIALS'));
+$client = new Client();
+$client->setAuthConfig($configJsonPath);
+$client->useApplicationDefaultCredentials();
+$client->setRedirectUri($base . '/auth/cb');
+// with refresh_token
+$client->setAccessType('offline');
+
+$auth_url = $client->createAuthUrl();
+header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
