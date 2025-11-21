6035f981fb7e78d6914a54938b2037bbb865c712
David Callizaya <davidcallizaya@gmail.com>
2025-11-21 17:44:43 -0400

    Add conditional branch support in symlink script for flexible plugin cloning

--- public/tce/symlink.sh
+++ public/tce/symlink.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+BRANCH=$1
+
 cd /opt/packages
-git clone https://github.com/ProcessMaker/package-plg.git
+
+if [ -n "$BRANCH" ]; then
+  git clone -b "$BRANCH" https://github.com/ProcessMaker/package-plg.git
+else
+  git clone https://github.com/ProcessMaker/package-plg.git
+fi
+
 cd /opt/processmaker/vendor/processmaker/
 rm -Rf package-plg
 ln -s /opt/packages/package-plg package-plg
