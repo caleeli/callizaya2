7b73985b76764670c181565e8b10f6107961ceed
David Callizaya <davidcallizaya@gmail.com>
2026-02-17 15:23:49 -0400

    more analisis

--- public/tce/monitor.sh
+++ public/tce/monitor.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 
 while true; do
-  date -u +"%Y-%m-%dT%H:%M:%SZ"
+  echo "---- $(date -u +'%F %T') ----"
   SCRIPT_NAME=/fpm-status \
   SCRIPT_FILENAME=/fpm-status \
   REQUEST_METHOD=GET \
-  cgi-fcgi -bind -connect 127.0.0.1:9000 \
-  | egrep 'listen queue:|idle processes:|active processes:|total processes:|max children reached:|slow requests:|accepted conn:'
-  echo "----"
+  cgi-fcgi -bind -connect 127.0.0.1:9000 2>/dev/null \
+    | egrep 'start since:|accepted conn:|listen queue:|max listen queue:|idle processes:|active processes:|total processes:|max active processes:|max children reached:|slow requests:'
   sleep 1
 done
