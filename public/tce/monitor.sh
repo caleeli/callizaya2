cb4334e77962c0f13d7a848ebb9ed8d13f85c06c
David Callizaya <davidcallizaya@gmail.com>
2026-02-17 12:27:21 -0400

    improve monitor

--- public/tce/monitor.sh
+++ public/tce/monitor.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
-SCRIPT_NAME=/fpm-status \
-SCRIPT_FILENAME=/fpm-status \
-REQUEST_METHOD=GET \
-cgi-fcgi -bind -connect 127.0.0.1:9000
+while true; do
+  date -u +"%Y-%m-%dT%H:%M:%SZ"
+  SCRIPT_NAME=/fpm-status \
+  SCRIPT_FILENAME=/fpm-status \
+  REQUEST_METHOD=GET \
+  cgi-fcgi -bind -connect 127.0.0.1:9000 \
+  | egrep 'listen queue:|idle processes:|active processes:|total processes:|max children reached:|slow requests:|accepted conn:'
+  echo "----"
+  sleep 1
+done
