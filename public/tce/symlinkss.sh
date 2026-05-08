fe9144a236fb54a1a5281b2a38feac70ba439f86
David Callizaya <davidcallizaya@gmail.com>
2026-05-08 18:42:26 -0400

    remove sudo

--- public/tce/symlinkss.sh
+++ public/tce/symlinkss.sh
@@ -1,21 +1,21 @@
 cd /opt/processmaker
-sudo -u nginx git fetch
-#sudo -u nginx cp composer.json storage/composer.json.bak
-#sudo -u nginx cp composer.lock storage/composer.lock.bak
-sudo -u nginx git restore composer.json composer.lock resources/js/requests/components/RequestScreens.vue resources/js/requests/components/screenDetail.vue resources/jscomposition/cases/casesDetail/components/DisplayForm.vue
-sudo -u nginx git fetch
-sudo -u nginx git checkout feature/FOUR-26166-b
-sudo -u nginx cp storage/composer.json.bak composer.json
-sudo -u nginx cp storage/composer.lock.bak composer.lock
-sudo -u nginx git branch
+git fetch
+#cp composer.json storage/composer.json.bak
+#cp composer.lock storage/composer.lock.bak
+git restore composer.json composer.lock resources/js/requests/components/RequestScreens.vue resources/js/requests/components/screenDetail.vue resources/jscomposition/cases/casesDetail/components/DisplayForm.vue
+git fetch
+git checkout feature/FOUR-26166-b
+cp storage/composer.json.bak composer.json
+cp storage/composer.lock.bak composer.lock
+git branch
 
 cd /opt/packages
-sudo -u nginx git clone --branch feature/FOUR-26166-integration https://github.com/ProcessMaker/package-savedsearch.git
+git clone --branch feature/FOUR-26166-integration https://github.com/ProcessMaker/package-savedsearch.git
 cd /opt/processmaker/vendor/processmaker
 rm -rf package-savedsearch
 ln -s /opt/packages/package-savedsearch package-savedsearch
 
 
 cd /opt/processmaker
-sudo -u nginx composer config --global --auth http-basic.processmaker.repo.packagist.com token 0df7b369d295c842dae46a40f7b694399e9f5d1b45d167e807afdb26424d
-sudo -u nginx composer require justinrainbow/json-schema:^6.5
+composer config --global --auth http-basic.processmaker.repo.packagist.com token 0df7b369d295c842dae46a40f7b694399e9f5d1b45d167e807afdb26424d
+composer require justinrainbow/json-schema:^6.5
